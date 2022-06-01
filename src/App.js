b4d7684b965ff459a19cb19bb69f58cbd426e038
ahamed <ahamedsha@outlook.in>
2022-06-01 14:34:14 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+
+    React.useEffect(() => {
+        console.log('Hello World!')
+    })
+  return (
+    <div>
+        hello
+    </div>
+  );
+}
+
+export default App;
